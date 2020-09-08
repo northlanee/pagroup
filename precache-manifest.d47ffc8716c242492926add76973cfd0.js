c5860cc96db1bd01c110aaacfa7a2ad347213a7e
northlanee <iwasmusic16@gmail.com>
2020-09-08 14:37:14 +0300

    Updates

---
+++ precache-manifest.d47ffc8716c242492926add76973cfd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7b93c2e41245a0d368ee93de8c8657",
+    "url": "/pagroup/index.html"
+  },
+  {
+    "revision": "7d0b01f06412e4bb96aa",
+    "url": "/pagroup/static/css/main.23085e98.chunk.css"
+  },
+  {
+    "revision": "01d17a6436acd00ea03e",
+    "url": "/pagroup/static/js/2.6e53be8b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pagroup/static/js/2.6e53be8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0b01f06412e4bb96aa",
+    "url": "/pagroup/static/js/main.228fa977.chunk.js"
+  },
+  {
+    "revision": "becb3f4f0284eae6dde8",
+    "url": "/pagroup/static/js/runtime-main.2fd830f0.js"
+  }
+]);
